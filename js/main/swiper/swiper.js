cafbbbefa7eda1efa4e43d36b629f41bdc3d90db
AleksandrSanduliak <lethalzmaneling@gmail.com>
2023-01-22 17:08:03 +0300

    add hero, swiper js

---
+++ js/main/swiper/swiper.js
@@ -0,0 +1,10 @@
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.slider', {
+    slidesPerView: 'auto',
+    slidesPerView: 1,
+    speed: 600,
+    autoplay: {
+        delay: 5000,
+      },
+});
